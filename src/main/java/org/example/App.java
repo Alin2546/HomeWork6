b2a95874c2976d0af17cab09ce982647c9500f8e
Alin2546 <eternalsq1337%gmail.com>
2024-12-03 15:30:49 +0200

    first steps

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,27 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.*;
+
+public class App {
+    /**
+     * The entry point of the program
+     * @param args which are not used here
+     */
+    public static void main(String[] args) {
+        Person pers = new Person("Bogdan", 31);
+
+        // Creating a TreeSet with a custom comparator which compares values based on name then by age
+        Set<Person> set = new TreeSet<>(Person.compareByNameThenByAge);
+
+        // Added the person instance in the TreeSet
+        set.add(pers);
+
+        for (Person person : set) {
+            System.out.println(person.getName() + " " + person.getAge());
+        }
+
+
+
+
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //    static Comparator<Person> compareByName = Comparator.comparing(Person :: getName);
+    //    static Comparator<Person> compareByAge = Comparator.comparing(Person :: getAge);
+
+    static Comparator<Person> compareByNameThenByAge = (o1, o2) -> {
+        if (!(o1.getName().equals(o2.getName()))) {
+            return o1.getName().compareTo(o2.getName());
+        }
+        return Integer.compare(o1.getAge(), o2.getAge());
+    };
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return getAge() == person.getAge() && Objects.equals(getName(), person.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
+    }
+
+    boolean isEqual(int x, int y) {
+        return x == y;
+    }
+
+
+}
